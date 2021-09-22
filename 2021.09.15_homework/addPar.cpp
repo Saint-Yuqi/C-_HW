054429235519e186c0f9399918f26eb76c8de3fe
Saint-Yuqi <yangyuqi2020@qq.com>
2021-09-23 01:37:39 +0800

    Update addPar.cpp

--- 2021.09.15_homework/addPar.cpp
+++ 2021.09.15_homework/addPar.cpp
@@ -1,46 +1,51 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
-string addPar(string n = "", string newS = "", int a = 0)
+void addPar(string n = "", int i = 0)
 {
-    int l = n.length();
+    int l = 0;
+    l = n.length();
 
-    if (a == l)
+    if(i < l)
     {
-        if(l % 2 == 1)
+        if (l % 2 == 0)
         {
-            return newS + ")";
+            if(i < l / 2 )
+            {
+            
+                cout << '(' << n[i];
+                addPar(n, i + 1);
+            }
+            else
+            {
+                cout << n[i] << ")";
+                addPar(n, i + 1);
+            }
         }
         else
         {
-            return newS;
+           if(i <= l / 2 )
+           {          
+                cout << "(" << n[i];
+                if(i == (l - 1)/2)
+                {
+                    cout << ")";
+                }
+                addPar(n, i + 1);
+           } 
+           else
+           {
+               cout << n[i] << ")";
+               addPar(n, i + 1);
+           }
         }
+        
+
     }
     
-    if(l % 2 == 1)
-    {
-        if(a < (l + 1) / 2)
-        {
-            return addPar(n, newS + "(" + n[a], a + 1);
-        }
-        else
-        {
-            return addPar(n, newS + ")" + n[a], a + 1);
-        }
-    }
-    if(l % 2 == 0)
-    {
-        if(a < l / 2)
-        {
-            return addPar(n, newS + "(" + n[a], a + 1);
-        }
-        else
-        {
-            return addPar(n, newS + n[a] + ")", a + 1);
-        }
-    }
-    return n;
+    return;
 }
 
 int main()
@@ -49,7 +54,9 @@ int main()
 
     cout << "Please input a string!" << endl;
     cin >> text;
-    cout << addPar(text) << endl;
-    
-    return 0;
+    addPar(text);
+
+    cout << endl;
+
+    return EXIT_SUCCESS;
 }
